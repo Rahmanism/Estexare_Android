316f15582b8c0338add82a1d85ff677eaabecbed
Rahmanism <s.mostafa.rahmani@gmail.com>
2019-06-30 23:40:07 +0430

    First run in a mobile was successful :).

---
+++ app/src/main/java/ir/rahmanism/estexare/EstexareDB.kt
@@ -0,0 +1,5 @@
+package ir.rahmanism.estexare
+
+class EstexareDB {
+
+}
--- app/src/main/java/ir/rahmanism/estexare/MainActivity.kt
+++ app/src/main/java/ir/rahmanism/estexare/MainActivity.kt
@@ -1,6 +1,7 @@
 package ir.rahmanism.estexare
 
 import android.os.Bundle
+import android.widget.Button
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.TextView
@@ -8,18 +9,42 @@ import android.widget.TextView
 class MainActivity : AppCompatActivity() {
 
     private lateinit var textMessage: TextView
+    private lateinit var resultTxt: TextView
+    private lateinit var sureLabelTxt: TextView
+    private lateinit var sureTxt: TextView
+    private lateinit var ayeLabelTxt: TextView
+    private lateinit var ayeNoTxt: TextView
+    private lateinit var ayeTxt: TextView
+    private lateinit var commentTxt: TextView
+    private lateinit var newBtn: Button
+    private var notLoadedYet: Boolean = true
+
     private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_fa -> {
                 textMessage.setText(R.string.title_home)
+                if (notLoadedYet) {
+                    sureLabelTxt.setText(R.string.sure_fa)
+                    sureTxt.setText(R.string.placeholder_sure_fa)
+                    ayeLabelTxt.setText(R.string.aye_fa)
+                    ayeNoTxt.setText(R.string.placeholder_aye_no_fa)
+                    ayeTxt.setText(R.string.placeholder_aye_fa)
+                    commentTxt.setText(R.string.placeholder_comment_fa)
+                    newBtn.setText(R.string.new_estexare_fa)
+                }
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_dashboard -> {
-                textMessage.setText(R.string.title_dashboard)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_notifications -> {
-                textMessage.setText(R.string.title_notifications)
+            R.id.navigation_en -> {
+                textMessage.setText(R.string.title_home)
+                if (notLoadedYet) {
+                    sureLabelTxt.setText(R.string.sure_en)
+                    sureTxt.setText(R.string.placeholder_sure_en)
+                    ayeLabelTxt.setText(R.string.aye_en)
+                    ayeNoTxt.setText(R.string.placeholder_aye_no_en)
+                    ayeTxt.setText(R.string.placeholder_aye_en)
+                    commentTxt.setText(R.string.placeholder_comment_en)
+                    newBtn.setText(R.string.new_estexare_en)
+                }
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -31,7 +56,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
-        textMessage = findViewById(R.id.message)
+        textMessage = findViewById(R.id.title_text)
+        resultTxt = findViewById(R.id.result)
+        sureLabelTxt = findViewById(R.id.sure_label)
+        sureTxt = findViewById(R.id.sure)
+        ayeLabelTxt = findViewById(R.id.aye_label)
+        ayeNoTxt = findViewById(R.id.aye_no)
+        ayeTxt = findViewById(R.id.aye)
+        commentTxt = findViewById(R.id.comment)
+        newBtn = findViewById(R.id.new_estexare_btn)
+
         navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
     }
 }
